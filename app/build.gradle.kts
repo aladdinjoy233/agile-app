ac4cefb987b44db14188d028285024e64a8c1e4a
Allan Chica <ninjalover249@gmail.com>
2024-02-28 16:55:03 -0300

    Create product form fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ app/src/main/java/com/example/agile/ui/primary/ui/products/FormProductViewModel.java
@@ -0,0 +1,7 @@
+package com.example.agile.ui.primary.ui.products;
+
+import androidx.lifecycle.ViewModel;
+
+public class FormProductViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
